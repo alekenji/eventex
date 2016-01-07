28b6c495eda645db37a624611afc80445c54ee3c
Alexandre Kenji <alekenji@gmail.com>
2016-01-07 15:58:50 -0200

    Adiciona teste na home

--- eventex/urls.py
+++ eventex/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
+from eventex.core import views as core_views
+
 
 urlpatterns = [
-    url(r'^$', 'eventex.core.views.home'),
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', core_views.home),
+    url(r'^admin/', include(admin.site.urls)),
 ]
